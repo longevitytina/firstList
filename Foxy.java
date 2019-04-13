0926c54cad43554599442e2970f54c93c2b18ccb
Tina <tina@Tinas-MacBook-Air-2.local>
2019-04-12 23:18:51 -0700

    Foxy.js

--- Foxy.java
+++
@@ -1,7 +0,0 @@
-//Opening line
-console.log(5);
-console.log('Foxy thang'); //prints Foxy thang
-console.log(34*5); //multiplies 
-console.log('foxy'.length); //prints 4
-
-
---
+++ Foxy.js
@@ -0,0 +1,17 @@
+//Opening line
+console.log(5);
+console.log('Foxy thang'); //prints Foxy thang
+console.log(34*5); //multiplies 
+console.log('foxy'.length); //prints 4
+console.log('foxy'.toUpperCase()); // Prints 'FOXY'
+console.log('foxy'.startsWith('F')); // Prints true
+console.log('     foxtrot     '.trim()); //removes empty space
+
+console.log(Math.floor(Math.random() * 100));//creates random number, multiples by 100 and rounds to whole number 
+console.log(Math.ceil(43.8)); //Returns the number of leading zeroes of a 32-bit integer.
+console.log(number.isInteger(2017)); //built-in Number object that checks if a number is an integer.
+
+
+
+
+
