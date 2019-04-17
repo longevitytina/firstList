e8ecf08b6e0ccba89a30e3695cc4f3edd25d3049
Tina <tina@Tinas-Air-2.attlocal.net>
2019-04-17 13:40:59 -0700

    practice uploading

--- Foxy.js
+++ Foxy.js
@@ -9,8 +9,12 @@ console.log('     foxtrot     '.trim()); //removes empty space
 
 console.log(Math.floor(Math.random() * 100));//creates random number, multiples by 100 and rounds to whole number 
 console.log(Math.ceil(43.8)); //Returns the number of leading zeroes of a 32-bit integer.
-console.log(number.isInteger(2017)); //built-in Number object that checks if a number is an integer.
 
+alert('Boo!')
+alert('Boo!')
+alert('Boo!')
+alert('Boo!')
+boo
 
 
 
