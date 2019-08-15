5ae0acceb4c47b9536107298004221b03b4d6f6c
Tina <longevity.tina@gmail.com>
2019-08-15 11:28:03 -0700

    update

--- objectsIntro.js
+++ objectsIntro.js
@@ -11,3 +11,21 @@ textSize(20);
 text(recipe.title, 10, 23);
 text("Serves: " + recipe.servings, 10, 55);
 text("Ingredients: " + recipe.ingredients, 10, 85);
+
+var paintBrush = {
+    x: 130,
+    y: 126,
+    img: getImage("avatars/duskpin-seed")
+};
+
+var paintCanvas = function() {
+    imageMode(CENTER);
+    image(paintBrush.img, paintBrush.x, paintBrush.y);
+};
+
+
+mouseMoved = function() {
+    paintBrush.x = mouseX;
+    paintBrush.y = mouseY;
+    paintCanvas();
+};
